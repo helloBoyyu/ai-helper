59145e574423a46f8b3ce51f527b8b6645e994c8
大宝的宝贝肥 <dabaodebaobeifei@dabaodebaobeifeideMac-mini.local>
2026-04-25 22:50:43 +0800

    inital commit

---
+++ src/main/java/org/example/aicoderhelper/ai/AiCodeHelperService.java
@@ -0,0 +1,9 @@
+package org.example.aicoderhelper.ai;
+
+import dev.langchain4j.service.SystemMessage;
+
+public interface AiCodeHelperService {
+
+    @SystemMessage(fromResource = "system-prompt.txt")
+    String chat(String userMessage);
+}
---
+++ src/test/java/org/example/aicoderhelper/ai/AiCodeHelperTest.java
@@ -0,0 +1,30 @@
+package org.example.aicoderhelper.ai;
+
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AiCodeHelperTest {
+    @Resource
+    private AiCodeHelper aiCodeHelper;
+
+    @Test
+    void chat() {
+        aiCodeHelper.chat("你好，我是虞泽贤！");
+    }
+
+    @Test
+    void testChat() {
+        UserMessage userMessage = UserMessage.from(
+                TextContent.from("描述图片"),
+                ImageContent.from("https://www.codefather.cn/logo.png")
+        );
+        aiCodeHelper.chat(userMessage);
+    }
+}
